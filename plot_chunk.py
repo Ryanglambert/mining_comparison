fd7e96c942665de4bc1045c86754af674f3e2be8
ryanglambert <ryan.g.lambert@gmail.com>
2015-09-09 13:45:17 -0700

    plot blocks almost working

--- plot_chunk.py
+++ plot_chunk.py
@@ -3,44 +3,52 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 plot_dict = {
-        'diamond':('b', 'o')
+        'diamond':('blue', 'D'),
+        'gold':('yellow', 'D'),
+        'iron':('grey', 'D'),
+        'redstone':('red', 'D'),
+        'stone':('white', 's'),
+        'gravel':('white', 's'),
+        'bedrock':('black', 's'),
         }
 
-def plot_blocks(chunk_blocks):
-    #fig = plt.figure()
-    #ax = fig.add_subplot(111, projection='3d')
-    #n = 100
-    #for c, m, zl, zh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
-        #xs = randrange(n, 23, 32)
-        #ys = randrange(n, 0, 100)
-        #zs = randrange(n, zl, zh)
-        #xs = 
-        #ys = 
-        #zs = 
-        #ax.scatter(xs, ys, zs, c=c, marker=m)
-    
-    #ax.set_xlabel('X Label')
-    #ax.set_ylabel('Y Label')
-    #ax.set_zlabel('Z Label')
-    
-    #plt.show()
-    pass
 
 def convert_block_id_to_scatter_color(block):
     block[0] = plot_dict[block[0]]
-    print block
     return block
 
+def convert_block_id_array_to_scatter_color_array(block_array):
+    for block_index in xrange(len(block_array)):
+        block_array[block_index] = convert_block_id_to_scatter_color(block_array[block_index])
 
-chunk_blocks = [
-        ['diamond', 4, 3, 5], 
-        ['diamond', 3, 3, 2], 
-        ['diamond', 1, 3, 2], 
-        ['diamond', 0, 8, 2], 
-        ['diamond', 10, 3, 20], 
+#block_array
+def plot_blocks(block_array_to_plot):
+    assert type(block_array_to_plot) == list
+    ## need to add how to decide which type to plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for color, marker in [
+            plot_dict['diamond'],
+            ]:
+        xs = [i[1] for i in block_array_to_plot]
+        ys = [j[2] for j in block_array_to_plot]
+        zs = [k[3] for k in block_array_to_plot]
+        ax.scatter(xs, ys, zs, color=color, marker=marker)
+    
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Z Label')
+    ax.set_zlabel('Y Label')
+    
+    plt.show()
+
+blocks = [
+        ['stone', 0, 8, 2], 
+        ['bedrock', 10, 3, 20], 
         ['diamond', 31, 39, 21]
         ]
 
+#convert_block_id_array_to_scatter_color_array(chunk_blocks)
+plot_blocks(blocks)
 
-block = ['diamond', 1, 3, 2]
-convert_block_id_to_scatter_color(block)
+
--- tests/convert_block_test.py
+++ tests/convert_block_test.py
@@ -1,6 +1,7 @@
 from nose.tools import *
 from plot_chunk import *
 
+hold_plot_dict = plot_dict
 def setup(): 
     global plot_dict
     plot_dict = {
@@ -9,15 +10,28 @@ def setup():
     print "setup!"
 
 def teardown():
-    #del plot_dict
+    plot_dict = hold_plot_dict
     print "TEAR DOWN!"
 
 def test_convert_block():
     block = ['diamond', 1, 3, 2]
     converted_block = convert_block_id_to_scatter_color(block)
-    assert_equal(
-            [('b', 'o'), 1, 3, 2], converted_block)
+    assert_equal([plot_dict['diamond'], 1, 3, 2], converted_block)
+
+def test_convert_block_array():
+    block_array = [
+            ['diamond', 2, 2, 3],
+            ['diamond', 1, 2, 0],
+            ['diamond', 5, 5, 5]
+            ]
+    converted_array = convert_block_id_array_to_scatter_color_array(block_array)
+    desired_array = [
+            [plot_dict['diamond'], 2, 2, 3],
+            [plot_dict['diamond'], 1, 2, 0],
+            [plot_dict['diamond'], 5, 5, 5]
+            ]
 
 setup()
 test_convert_block()
+test_convert_block_array()
 teardown()
