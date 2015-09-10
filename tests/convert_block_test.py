6dc50280eb0155319b841d33a5517160c920f1da
ryanglambert <ryan.g.lambert@gmail.com>
2015-09-10 15:45:14 -0700

    test of getting subset

--- tests/convert_block_test.py
+++ tests/convert_block_test.py
@@ -1,16 +1,10 @@
 from nose.tools import *
 from plot_chunk import *
 
-hold_plot_dict = plot_dict
 def setup(): 
-    global plot_dict
-    plot_dict = {
-            'diamond':('b', 'o')
-            }
     print "setup!"
 
 def teardown():
-    plot_dict = hold_plot_dict
     print "TEAR DOWN!"
 
 def test_convert_block():
@@ -18,20 +12,25 @@ def test_convert_block():
     converted_block = convert_block_id_to_scatter_color(block)
     assert_equal([plot_dict['diamond'], 1, 3, 2], converted_block)
 
-def test_convert_block_array():
+
+def test_get_subset_of_type():
     block_array = [
             ['diamond', 2, 2, 3],
-            ['diamond', 1, 2, 0],
-            ['diamond', 5, 5, 5]
+            ['diamond', 2, 2, 8],
+            ['stone', 1, 2, 0],
+            ['diamond', 0, 2, 3],
+            ['bedrock', 5, 5, 5]
             ]
-    converted_array = convert_block_id_array_to_scatter_color_array(block_array)
     desired_array = [
-            [plot_dict['diamond'], 2, 2, 3],
-            [plot_dict['diamond'], 1, 2, 0],
-            [plot_dict['diamond'], 5, 5, 5]
+            ['diamond', 2, 2, 3],
+            ['diamond', 2, 2, 8],
+            ['diamond', 0, 2, 3]
             ]
+
+    subset = get_subset_of_type(block_array, 'diamond')
+    assert_equal(subset, desired_array)
 
 setup()
 test_convert_block()
-test_convert_block_array()
+test_get_subset_of_type()
 teardown()
