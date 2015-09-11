a0e7836f90c3a0fca7de7df93b23dfaa6540b6df
ryanglambert <ryan.g.lambert@gmail.com>
2015-09-11 13:49:39 -0700

    these tests are out of date

--- tests/1dto3d_chunk_test.py
+++ tests/1dto3d_chunk_test.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-from plot_chunk import *
+from block_handler import get_subset_of_type
 
 def setup(): 
     print "setup!"
@@ -8,18 +8,18 @@ def teardown():
     print "TEAR DOWN!"
 
 def test_get_subset_of_type():
-    block_array = [
-            ['diamond', 2, 2, 3],
-            ['stone',1,1,1],
-            ['diamond', 2, 2, 8],
-            ['diamond', 0, 2, 3],
-            ['stone',1,1,2]
-            ]
-    desired_array = [
-            ['diamond', 2, 2, 3],
-            ['diamond', 2, 2, 8],
-            ['diamond', 0, 2, 3]
-            ]
+    block_array = {
+            (2,2,3):'diamond',
+            (1,1,1):'stone',
+            (2,2,8):'diamond',
+            (0,2,3):'diamond',
+            (1,1,2):'stone',
+            }
+    desired_array = {
+            (2,2,3):'diamond',
+            (2,2,8):'diamond',
+            (0,2,3):'diamond',
+            }
     filtered_array = get_subset_of_type(block_array, 'diamond')
 
     assert_equal(filtered_array, desired_array)
