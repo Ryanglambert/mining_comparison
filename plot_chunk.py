239b76948429df819e8afa370eaa073ce37eddde
ryanglambert <ryan.g.lambert@gmail.com>
2015-09-11 13:49:14 -0700

    moved get subset into block handler

--- plot_chunk.py
+++ plot_chunk.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import block_handler
 
 """
 takes dict of blocks in format below and plots according to plot_dict into a 3d scatter plot
@@ -12,24 +13,16 @@ block_dict = {
 """
 
 plot_dict = {
-        'diamond':('blue', 'D'),
-        'gold':('yellow', 'D'),
-        'iron':('white', 'D'),
-        'coal':('white', 'D'),
-        'redstone':('red', 'D'),
-        'stone':('white', 's'),
-        'dirt':('white', 's'),
-        'gravel':('white', 's'),
-        'bedrock':('white', 's'),
+        'diamond':('blue', 'h'),
+        'gold':('yellow', 'h'),
+        'iron':('white', 'h'),
+        'coal':('white', 'h'),
+        'redstone':('red', 'h'),
+        #'stone':('white', '.'),
+        #'dirt':('white', '.'),
+        #'gravel':('white', '.'),
+        #'bedrock':('white', '.'),
         }
-
-
-def get_subset_of_type(unfiltered_block_array, block_type):
-    subset_dict = {}
-    for key in unfiltered_block_array:
-        if unfiltered_block_array[key] == block_type:
-            subset_dict[key] = block_type
-    return subset_dict
 
 
 def plot_blocks(block_array_to_plot):
@@ -39,7 +32,7 @@ def plot_blocks(block_array_to_plot):
 
     for key in plot_dict.keys():
         try:
-            array_subset = get_subset_of_type(block_array_to_plot, key)
+            array_subset = block_handler.get_subset_of_type(block_array_to_plot, key)
             xs = [i[0] for i in array_subset.keys()]
             zs = [j[1] for j in array_subset.keys()]
             ys = [k[2] for k in array_subset.keys()]
@@ -55,9 +48,9 @@ def plot_blocks(block_array_to_plot):
 
 def main():
     blocks = {}
-    for y in range(0,16):
-        for z in range(0,16):
-            for x in range(0,16):
+    for y in range(0,32):
+        for z in range(0,32):
+            for x in range(0,32):
                 blocks[(x, z, y)] = 'stone'
     """
     x = math.floor(i % x_length)
@@ -66,15 +59,11 @@ def main():
     """
     #pdb.set_trace()
     blocks[(5,5,5)] = 'diamond'
+    blocks[(21,5,20)] = 'diamond'
+    blocks[(16,5,16)] = 'diamond'
+    blocks[(17,5,18)] = 'diamond'
     #print blocks[(5,5,5)]
 
-    #blocks = [
-            #['stone', 0, 8, 2], 
-            #['bedrock', 10, 3, 20], 
-            #['diamond', 31, 39, 21],
-            #['diamond', 3, 39, 21],
-            #['diamond', 31, 9, 21],
-            #]
     
     plot_blocks(blocks)
 
